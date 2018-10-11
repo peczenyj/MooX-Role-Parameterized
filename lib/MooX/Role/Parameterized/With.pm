f1cc91c5f7c3164fe87a9ee8d585641a7043a67b
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-10-11 12:00:16 +0700

    Add a with keyword
    The syntax is compatible with Moose's with.
    
    This commit builds upon PR#6.

--- lib/MooX/Role/Parameterized/With.pm
+++ lib/MooX/Role/Parameterized/With.pm
@@ -4,18 +4,35 @@ use warnings;
 
 # ABSTRACT: MooX::Role::Parameterized:With - dsl to apply roles with composition parameters
 
-use Exporter;    # qw(import);
+use Exporter        qw(import);
 use Module::Runtime qw(use_module);
-use List::MoreUtils qw(natatime);
+use Moo::Role       qw();
+use Role::Tiny      qw();
 
-sub import {
-    my $package = shift;
-    my $target  = caller;
+our @EXPORT = qw(with);
 
-    my $it = natatime( 2, @_ );
+sub with {
+    my $target = caller;
 
-    while ( my ( $role, $params ) = $it->() ) {
-        use_module($role)->apply( $params, target => $target );
+    while (@_) {
+        my $role = shift;
+        use_module($role);
+        if (@_ && ref $_[0] eq 'HASH') {
+            my $params = shift;
+            $role->apply( $params, target => $target );
+        } else {
+            if ($role->can("apply")) {
+                $role->apply( {}, target => $target );
+            } elsif (Moo::Role->is_role($role)) {
+                Moo::Role->apply_roles_to_package( $target, $role );
+                Moo::Role->_maybe_reset_handlemoose($target);
+            } elsif (Role::Tiny->is_role($role)) {
+                Role::Tiny->apply_roles_to_package( $target, $role );
+            } else {
+                die "Can't apply $role to $target: $role is neither a ".
+                    "MooX::Role::Parameterized/Moo::Role/Role::Tiny role";
+            }
+        }
     }
 }
 
@@ -33,7 +50,7 @@ MooX::Role::Parameterized:With - dsl to apply roles with composition parameters
 
     use Moo;
     use MooX::Role::Parameterized::With Bar => {
-        attr => 'baz', 
+        attr => 'baz',
         method => 'run'
     }, Other::Role => { ... };
 
@@ -44,7 +61,7 @@ MooX::Role::Parameterized:With - dsl to apply roles with composition parameters
 This B<experimental> package try to offer an easy way to add parametrized roles.
 
 Will load and apply L<MooX::Roles::Parameterized> roles, just need use this package
-with a hash of role => parameters. 
+with a hash of role => parameters.
 
 =head1 AUTHOR
 
@@ -53,4 +70,3 @@ Tiago Peczenyj <tiago (dot) peczenyj (at) gmail (dot) com>
 =head1 BUGS
 
 Please report any bugs or feature requests on the bugtracker website
-
