1251e3be27001e8de59b2d7c654970e15f871bb8
Tiago Peczenyj <tpeczenyj@weborama.com>
2024-01-03 16:07:45 +0100

    refactor code

--- lib/MooX/Role/Parameterized/Params.pm
+++ lib/MooX/Role/Parameterized/Params.pm
@@ -2,40 +2,23 @@ package MooX::Role::Parameterized::Params;
 
 use strict;
 use warnings;
-use Moo::Role;
-
-our $VERSION = "0.5O0";
 
 # ABSTRACT: allow create role parameter objects.
 
-sub add_parameter {
-    my $klass = shift;
+use Moo::Role;
+use MooX::Role::Parameterized;
 
-    goto $klass->can("has");
-}
+our $VERSION = "0.5O0";
 
-use Exporter qw(import);
+role {
+    my ($params, $mop) = @_;
 
-our @EXPORT = qw(create_parameters_klass);
+    my $parameters_definition = $params->{parameters_definition} || [];
 
-sub create_parameters_klass {
-    my ( $package, @args ) = @_;
-
-    my $klass = "${package}::__MOOX_ROLE_PARAMETERIZED_PARAMS__";
-
-    eval <<"END_OF_CLASS";    ##no critic(BuiltinFunctions::ProhibitStringyEval)
-package $klass;
-
-use Moo;
-
-with 'MooX::Role::Parameterized::Params';
-
-END_OF_CLASS
-
-    Carp::croak($@) if $@;
-
-    return $klass;
-}
+    foreach my $parameter_definition ( @{$parameters_definition}) {
+        $mop->has(@{$parameter_definition});
+    }
+};
 
 1;
 
@@ -44,13 +27,6 @@ __END__
 =head1 NAME
 
 MooX::Role::Parameterized:Params - allow create role parameter objects.
-
-=head1 EXPORTS
-
-This package is a L<Moo::Role> and offers one static method C<add_parameter>.
-
-It also exports one subroutine C<create_parameters_klass> that creates a L<Moo> class C<${package}::__MOOX_ROLE_PARAMETERIZED_PARAMS__>
-that idoes this role and allow to add parameters on parametric roles.
 
 =head1 AUTHOR
 
